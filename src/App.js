1cd32155a98eec2a0672e5368319ce0a4572e8e8
Yan Sidyakin <sidyakinian@gmail.com>
2024-10-26 14:27:01 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React at HackCamp
         </a>
       </header>
     </div>
